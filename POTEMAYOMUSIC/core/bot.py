9f065d814b3777e55ead595a55120a74143c00c3
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-23 14:01:55 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/core/bot.py
+++ POTEMAYOMUSIC/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class JARVIS(Client):
+class TOXIC(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Annie is on the way...")
+        LOGGER(__name__).info(f"Potemayo is on the way...")
         super().__init__(
-            name="𝐀𝐍𝐍𝐈𝐄 𝐗 𝐌𝐔𝐒𝐈𝐂",
+            name="POTEMAYO MUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
