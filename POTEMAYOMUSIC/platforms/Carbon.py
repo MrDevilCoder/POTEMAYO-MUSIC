151e1d4f6d23a8aeb205850f5a42463746d46ee5
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-23 21:00:05 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/platforms/Carbon.py
+++ POTEMAYOMUSIC/platforms/Carbon.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 import random
 from os.path import realpath
 
