930fcbeac7c939ad6bde4d39fad50d4bba90877c
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-24 21:34:13 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/utils/database/memorydatabase.py
+++ POTEMAYOMUSIC/utils/database/memorydatabase.py
@@ -1,19 +1,9 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import json
 import os
 from typing import Dict, List, Union
 
 import config
-from ANNIEMUSIC.core.mongo import mongodb
+from POTEMAYOMUSIC.core.mongo import mongodb
 
 channeldb = mongodb.cplaymode
 commanddb = mongodb.commands
