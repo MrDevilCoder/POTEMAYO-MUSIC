ef4171c1f33421d9cf4e1a8ddc2a85d56a71f6c9
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-24 21:36:08 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/utils/database/mongodatabase.py
+++ POTEMAYOMUSIC/utils/database/mongodatabase.py
@@ -1,16 +1,6 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from typing import Dict, List, Union
 
-from ANNIEMUSIC.core.mongo import mongodb
+from POTEMAYOMUSIC.core.mongo import mongodb
 
 queriesdb = mongodb.queries
 userdb = mongodb.userstats
