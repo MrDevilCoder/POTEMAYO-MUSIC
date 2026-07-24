3c2f5c480d8c3c76662c87e355d7db9a374f9390
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-24 11:51:01 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/plugins/admins/resume.py
+++ POTEMAYOMUSIC/plugins/admins/resume.py
@@ -1,21 +1,11 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
-from ANNIEMUSIC import app
-from ANNIEMUSIC.core.call import ANNIE as ANNIE
-from ANNIEMUSIC.utils.database import is_music_playing, music_on
-from ANNIEMUSIC.utils.decorators import AdminRightsCheck
+from POTEMAYOMUSIC import app
+from POTEMAYOMUSIC.core.call import TOXIC as TOXIC
+from POTEMAYOMUSIC.utils.database import is_music_playing, music_on
+from POTEMAYOMUSIC.utils.decorators import AdminRightsCheck
 
 
 @app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
@@ -24,7 +14,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await ANNIE.resume_stream(chat_id)
+    await TOXIC.resume_stream(chat_id)
     buttons_resume = [
         [
             InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
