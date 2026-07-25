b5019c1303bb6a4b692839d54b57766b6fd83463
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-25 14:54:57 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/utils/inline/help.py
+++ POTEMAYOMUSIC/utils/inline/help.py
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_GROUP
-from ANNIEMUSIC import app
+from POTEMAYOMUSIC import app
 
 
 def support_group_markup(_):
