da4dd6c34c8fe36884fc44d1b70b9e4e410ea785
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-26 21:50:48 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/utils/channelplay.py
+++ POTEMAYOMUSIC/utils/channelplay.py
@@ -1,16 +1,5 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-
-from ANNIEMUSIC import app
-from ANNIEMUSIC.utils.database import get_cmode
+from POTEMAYOMUSIC import app
+from POTEMAYOMUSIC.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
