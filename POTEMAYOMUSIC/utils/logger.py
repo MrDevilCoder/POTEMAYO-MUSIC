c82abfd6bbd13cc6772ee386f180fe7f3108f9bd
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-26 22:32:13 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/utils/logger.py
+++ POTEMAYOMUSIC/utils/logger.py
@@ -1,16 +1,6 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from config import LOG, LOG_GROUP_ID
-from ANNIEMUSIC import app
-from ANNIEMUSIC.utils.database import is_on_off
+from POTEMAYOMUSIC import app
+from POTEMAYOMUSIC.utils.database import is_on_off
 
 
 async def play_logs(message, streamtype):
