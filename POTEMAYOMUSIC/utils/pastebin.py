fb251e02fe6dac8754dbb9eb84404a51d2ca93f5
MrDevilCoder <mrdevilcoder@gmail.com>
2026-07-26 22:33:21 +0530

    DEVILCODER✨

--- POTEMAYOMUSIC/utils/pastebin.py
+++ POTEMAYOMUSIC/utils/pastebin.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2024 by Moonshining1@Github, < https://github.com/Moonshining1 >.
-#
-# This file is part of < https://github.com/Moonshining1/ANNIE-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Moonshining1/ANNIE-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 import aiohttp
 
 BASE = "https://batbin.me/"
@@ -22,7 +13,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def ANNIEbin(text):
+async def TOXICbin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
