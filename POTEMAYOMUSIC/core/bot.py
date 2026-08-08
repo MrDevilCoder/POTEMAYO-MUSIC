1ade5b6fd155449cb6ca1937703a1f5ca6130836
MrDevilCoder <mrdevilcoder@gmail.com>
2026-08-08 15:56:58 +0530

    Update bot.py

--- POTEMAYOMUSIC/core/bot.py
+++ POTEMAYOMUSIC/core/bot.py
@@ -6,15 +6,16 @@ import config
 from ..logging import LOGGER
 
 
-class potemayo(Client):
+class Potemayo(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Potemayo is on the way...")
+        LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="POTEMAYO MUSIC",
+            name="POTEMAYOMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
             in_memory=True,
+            parse_mode=ParseMode.HTML,
             max_concurrent_transmissions=7,
         )
 
